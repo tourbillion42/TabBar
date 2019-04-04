2566cb56225f2e30c1fd66cedb26cf6afa32fb20
tourbillion42 <tourbillion42@gmail.com>
2019-04-05 06:01:37 +0900

    add  label for SecondView and ThirdView

--- tabBar/ThirdViewController.swift
+++ tabBar/ThirdViewController.swift
@@ -10,5 +10,17 @@ import UIKit
 
 class ThirdViewController : UIViewController {
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        title.text = "Third View"
+        title.textAlignment = .center
+        title.textColor = UIColor.green
+        title.font = UIFont.boldSystemFont(ofSize: 20)
+        title.sizeToFit()
+        title.center.x = self.view.frame.width / 2
+        
+        self.view.addSubview(title)
+    }
 }
